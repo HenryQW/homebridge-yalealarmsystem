f17b7cce792f3aa5f61d7ef320a1e2a15a6e112a
Jonathan Dann <jonathandann@users.noreply.github.com>
2019-08-14 11:09:29 +0100

    Replace LockMechanism Characteristic with SecuritySystemCharacteristic. Handle STAY_ARM and NIGHT_ARM states. These map to Yale's "Part Arm" mode, which the API exposes as "home". Handle STAY_ARM and NIGHT_ARM in YaleAlarm.getStatus() Replace console.log() with this.log()

--- index.js
+++ index.js
@@ -13,53 +13,111 @@ function YaleAlarm(log, config) {
     this.name = config["name"];
     this.config = config;
 
-    this.service = new Service.LockMechanism(this.name);
+    this.service = new Service.SecuritySystem(this.name);
+    this.service
+    .getCharacteristic(Characteristic.SecuritySystemCurrentState)
+    .on('get', this.getCurrentState.bind(this));
 
     this.service
-    .getCharacteristic(Characteristic.LockCurrentState)
-    .on('get', this.getState.bind(this));
+    .getCharacteristic(Characteristic.SecuritySystemTargetState)
+    .on('get', this.getTargetState.bind(this))
+    .on('set', this.setTargetState.bind(this));
 
-    this.service
-    .getCharacteristic(Characteristic.LockTargetState)
-    .on('get', this.getState.bind(this))
-    .on('set', this.setState.bind(this));
+    this.informationService = new Service.AccessoryInformation();
+
+    this.informationService
+    .setCharacteristic(Characteristic.Manufacturer, "Yale")
+    .setCharacteristic(Characteristic.Model, "Yale IA-320");
 }
 
-YaleAlarm.prototype.getState = function(callback) {
-    this.log("Getting current state...");
-
+YaleAlarm.prototype.getCurrentState = function(callback) {
+    this.log("getCurrentState()");
+    // callback(null, Characteristic.SecuritySystemCurrentState.DISARMED)
     getAccessToken(
         this.config.username, 
         this.config.password
     ).then(getStatus).then((response) => {
-        console.log(response);
-        callback(null, response === 'arm');
-    }).catch(console.log);
+        this.log(`Yale Response: getCurrentState() ${response}`);
+        var currentState;
+        if (response === "arm") {
+            this.log("Reporting current state as .AWAY_ARM");
+            currentState = Characteristic.SecuritySystemCurrentState.AWAY_ARM;
+        } else if (response === "disarm") {
+            this.log("Reporting current state as .DISARMED");
+            currentState = Characteristic.SecuritySystemCurrentState.DISARMED
+        } else { // "home"
+            this.log("Reporting current state as .NIGHT_ARM");
+            // HomeKit also exposes STAY_ARM. Yale doesn't distinguish between the concepts of "STAY_ARM" and "NIGHT_ARM"
+            // So we just arbitrarily always choose to map "home" <-> NIGHT_ARM.
+            currentState = Characteristic.SecuritySystemCurrentState.NIGHT_ARM 
+        }
+        callback(null, currentState);
+    }).catch(this.log);
 }
 
-YaleAlarm.prototype.setState = function(state, callback) {
-    var alarmState = (state == Characteristic.LockTargetState.SECURED) ? "arm" : "disarm";
+YaleAlarm.prototype.getTargetState = function(callback) {
+    this.log("getTargetState()");
+    getAccessToken(
+        this.config.username, 
+        this.config.password
+    ).then(getStatus).then((response) => {
+        this.log(`Yale Response: getCurrentState() ${response}`);
+        var currentState;
+        if (response === "arm") {
+            this.log("Reporting target state as .AWAY_ARM");
+            currentState = Characteristic.SecuritySystemTargetState.AWAY_ARM;
+        } else if (response === "disarm") {
+            this.log("Reporting target state as .DISARM");
+            currentState = Characteristic.SecuritySystemTargetState.DISARM
+        } else { // "home"
+            this.log("Reporting target state as .NIGHT_ARM");
+            // HomeKit also exposes STAY_ARM. Yale doesn't distinguish between the concepts of "STAY_ARM" and "NIGHT_ARM"
+            // So we just arbitrarily always choose to map "home" <-> NIGHT_ARM.
+            currentState = Characteristic.SecuritySystemTargetState.NIGHT_ARM 
+        }
+        callback(null, currentState);
+    }).catch(this.log);
+}
 
-    console.log(`Set Alarm state to ${alarmState}`);
+YaleAlarm.prototype.setTargetState = function(targetState, callback) {
+    var alarmState;
+    if (targetState === Characteristic.SecuritySystemTargetState.AWAY_ARM) {
+        alarmState = "arm";
+    } else if (targetState === Characteristic.SecuritySystemTargetState.DISARM) {
+        alarmState = "disarm";
+    } else { // .STAY_ARM || .NIGHT_ARM
+        alarmState = "home";
+    }
+    this.log(`Set Alarm state to ${alarmState}`);
 
     getAccessToken(
         this.config.username, 
         this.config.password
     ).then((accessToken) => {
         setStatus(accessToken, alarmState).then((response) => { 
-            var currentState = (state == Characteristic.LockTargetState.SECURED) ?
-            Characteristic.LockCurrentState.SECURED : Characteristic.LockCurrentState.UNSECURED;
-        
-            this.service.setCharacteristic(Characteristic.LockCurrentState, currentState);
-        
-            callback(null); // success
+            this.log(`Yale Response setState() ${response}`);
+            var currentState;
+            if (response === "OK") {
+                var currentState;
+                if (targetState === Characteristic.SecuritySystemTargetState.STAY_ARM) {
+                    currentState = Characteristic.SecuritySystemCurrentState.STAY_ARM;
+                } else if (targetState == Characteristic.SecuritySystemTargetState.AWAY_ARM) {
+                    currentState = Characteristic.SecuritySystemCurrentState.AWAY_ARM;
+                } else if (targetState == Characteristic.SecuritySystemTargetState.NIGHT_ARM) {
+                    currentState = Characteristic.SecuritySystemCurrentState.NIGHT_ARM;
+                } else if (targetState == Characteristic.SecuritySystemTargetState.DISARM) {
+                    currentState = Characteristic.SecuritySystemCurrentState.DISARMED;
+                }
+                this.service.setCharacteristic(Characteristic.SecuritySystemCurrentState, currentState);
+                callback(null); // success
+            }
         }).catch((response) => {
-            console.log('catch')
-            console.log(response)
+            this.log('catch')
+            this.log(response)
         });
     });
 }
 
 YaleAlarm.prototype.getServices = function() {
-    return [this.service];
+    return [this.service, this.informationService];
 }
